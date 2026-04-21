fbb7f13730fac7a026d1b6a9fda2496cad4d8701
MRYUNZUHH <mryunzuh@gmail.com>
2026-04-21 20:29:25 +0300

    Fix logout redirect

---
+++ frontend/public/fix.js
@@ -0,0 +1,14 @@
+﻿// Fix for logout redirect
+if (typeof window !== 'undefined') {
+  const originalPush = window.history.pushState;
+  window.history.pushState = function() {
+    return originalPush.apply(this, arguments);
+  };
+  
+  // Fix logout function
+  window.logout = function() {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('user');
+    window.location.replace('/login');
+  };
+}
